4783bc3c9bd393e2a42940e5d3aec981e7fdf932
abbasi012 <haseebsajjad666@gmail.com>
2024-09-18 23:16:01 +0500

    just create a app with create-rect and then vital

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <h1>chai aur React</h1>
+  );
+}
+
+export default App;
--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
  # react series with chai aur code
- 
+ npm i tailwindcss@3.1.8
